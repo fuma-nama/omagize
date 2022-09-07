d09f731f3edd20db85f9b47abe18e1227bf1fd6a
SonMooSans <god63820869@gmail.com>
2022-09-07 22:03:47 +0800

    Clean codes

---
+++ src/components/card/chat/MessageItem.tsx
@@ -0,0 +1,30 @@
+import {Message} from "api/MessageAPI";
+import {Avatar, Flex, HStack, SkeletonCircle, SkeletonText, Text, useColorModeValue} from "@chakra-ui/react";
+import {AvatarBg} from "variables/colors";
+
+export default function MessageItem(props: Message) {
+    const {author} = props
+    const secondaryText = useColorModeValue('gray.400', 'white');
+
+    return <Flex direction="row" _hover={{bg: "navy.800"}} p={7} transition="all 0.2s" rounded='xl'>
+        <Avatar name={author.username} src={author.avatar} bg={AvatarBg} />
+        <Flex direction='column' align='start' ml={2}>
+            <HStack>
+                <Text fontWeight='bold' fontSize='lg'>{author.username}</Text>
+                <Text textColor={secondaryText}>- {props.timestamp.toLocaleTimeString()}</Text>
+            </HStack>
+
+            <Text>{props.content}</Text>
+        </Flex>
+    </Flex>
+}
+
+export function MessageItemSkeleton(props: {noOfLines: number}) {
+    return <Flex direction="row" p={7} transition="all 0.2s" rounded='xl'>
+        <SkeletonCircle w="50px" h="50px" />
+        <Flex direction='column' align='start' ml={2} gap={5} w='full' maxW='500px'>
+            <SkeletonText w='46%' noOfLines={1} />
+            <SkeletonText w='full' noOfLines={props.noOfLines} />
+        </Flex>
+    </Flex>
+}

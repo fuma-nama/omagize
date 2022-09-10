22b58c781d3875bfc66a71e80e952ef52bfbe685
SonMooSans <god63820869@gmail.com>
2022-09-10 18:41:02 +0800

    Impl User Profile Banner

--- src/views/admin/profile/components/Banner.tsx
+++ src/views/admin/profile/components/Banner.tsx
@@ -1,58 +1,96 @@
 // Chakra imports
-import { Avatar, Box, Flex, Text, useColorModeValue } from '@chakra-ui/react';
+import {
+	Avatar,
+	Box,
+	Flex,
+	Grid,
+	SimpleGrid,
+	Skeleton,
+	SkeletonCircle,
+	SkeletonText,
+	Text,
+	useColorModeValue
+} from '@chakra-ui/react';
 import Card from 'components/card/Card';
+import {useUserQuery} from "../../../../api/UserAPI";
+import {useGroupsQuery} from "../../../../api/GroupAPI";
 
-export default function Banner(props: {
-	banner: string;
-	avatar: string;
-	name: string;
-	job: string;
-	posts: number | string;
-	followers: number | string;
-	following: number | string;
-	[x: string]: any;
-}) {
-	const { banner, avatar, name, job, posts, followers, following, ...rest } = props;
+function useColors() {
+
+	return {
+		textColorPrimary: useColorModeValue('secondaryGray.900', 'white'),
+		textColorSecondary: 'gray.400',
+		borderColor: useColorModeValue('white !important', '#111C44 !important')
+	}
+}
+export default function Banner(props: any) {
+	const query = useUserQuery()
+	const groupsQuery = useGroupsQuery()
+
 	// Chakra Color Mode
-	const textColorPrimary = useColorModeValue('secondaryGray.900', 'white');
-	const textColorSecondary = 'gray.400';
-	const borderColor = useColorModeValue('white !important', '#111C44 !important');
+	const {textColorPrimary, textColorSecondary, borderColor} = useColors()
+
+	if (query.isLoading) return <BannerSkeleton />
+	const user = query.data
+	const groups = groupsQuery.data
+
 	return (
-		<Card mb={{ base: '0px', lg: '20px' }} alignItems='center' {...rest}>
-			<Box bg={`url(${banner})`} bgSize='cover' borderRadius='16px' h='131px' w='100%' />
-			<Avatar mx='auto' src={avatar} h='87px' w='87px' mt='-43px' border='4px solid' borderColor={borderColor} />
+		<Card mb={{ base: '0px', lg: '20px' }} alignItems='center' {...props}>
+			<Box bgImg={user.bannerUrl} bgSize='cover' borderRadius='16px' h='131px' w='100%' />
+			<Avatar mx='auto' src={user.avatarUrl} h='87px' w='87px' mt='-43px' border='4px solid' borderColor={borderColor} />
 			<Text color={textColorPrimary} fontWeight='bold' fontSize='xl' mt='10px'>
-				{name}
+				{user.username}
 			</Text>
 			<Text color={textColorSecondary} fontSize='sm'>
-				{job}
+				{user.description}
 			</Text>
-			<Flex w='max-content' mx='auto' mt='26px'>
-				<Flex mx='auto' me='60px' alignItems='center' flexDirection='column'>
-					<Text color={textColorPrimary} fontSize='2xl' fontWeight='700'>
-						{posts}
-					</Text>
+			<SimpleGrid columns={2} mx='auto' mt='26px' gap={5}>
+				<Flex alignItems='center' flexDirection='column'>
+					{groups?
+						<Text color={textColorPrimary} fontSize='2xl' fontWeight='700'>
+							{groups.length}
+						</Text> :
+						<Skeleton w='60px' h='30px'/>
+					}
+
 					<Text color={textColorSecondary} fontSize='sm' fontWeight='400'>
-						Posts
+						Joined Groups
 					</Text>
 				</Flex>
-				<Flex mx='auto' me='60px' alignItems='center' flexDirection='column'>
+				<Flex alignItems='center' flexDirection='column'>
 					<Text color={textColorPrimary} fontSize='2xl' fontWeight='700'>
-						{followers}
+						{33}
 					</Text>
 					<Text color={textColorSecondary} fontSize='sm' fontWeight='400'>
-						Followers
+						Friends
 					</Text>
 				</Flex>
-				<Flex mx='auto' alignItems='center' flexDirection='column'>
-					<Text color={textColorPrimary} fontSize='2xl' fontWeight='700'>
-						{following}
-					</Text>
-					<Text color={textColorSecondary} fontSize='sm' fontWeight='400'>
-						Following
-					</Text>
-				</Flex>
-			</Flex>
+			</SimpleGrid>
 		</Card>
 	);
+}
+
+function BannerSkeleton(props: any) {
+	const {textColorSecondary, borderColor} = useColors()
+
+	return <Card mb={{ base: '0px', lg: '20px' }} alignItems='center' {...props}>
+		<Skeleton bgSize='cover' borderRadius='16px' h='131px' w='100%' />
+		<SkeletonCircle mx='auto' h='87px' w='87px' mt='-43px' border='4px solid' borderColor={borderColor} />
+		<Skeleton w='200px' h='30px' my='10px' />
+		<SkeletonText w='300px' />
+		<SimpleGrid columns={2} mx='auto' mt='26px' gap={5}>
+			<Flex alignItems='center' flexDirection='column'>
+				<Skeleton w='60px' h='30px' />
+				<Text color={textColorSecondary} fontSize='sm' fontWeight='400'>
+					Joined Groups
+				</Text>
+			</Flex>
+			<Flex alignItems='center' flexDirection='column'>
+				<Skeleton w='60px' h='30px'/>
+				<Text color={textColorSecondary} fontSize='sm' fontWeight='400'>
+					Friends
+				</Text>
+			</Flex>
+		</SimpleGrid>
+	</Card>
 }

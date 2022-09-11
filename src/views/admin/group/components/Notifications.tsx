5e625e0e018008449c4e9486ed51543de4090f80
SonMooSans <god63820869@gmail.com>
2022-09-11 16:55:24 +0800

    Add Skeleton to Notifications

--- src/views/admin/group/components/Notifications.tsx
+++ src/views/admin/group/components/Notifications.tsx
@@ -4,18 +4,12 @@ import {PageContext} from "contexts/PageContext";
 import {Button, Flex, Icon, Image, Text, useColorModeValue} from "@chakra-ui/react";
 
 import Card from "components/card/Card";
-import { GroupNotificationItem } from "components/card/Notification";
+import {GroupNotificationItem, NotificationSkeleton} from "components/card/notification/Notification";
 
 export function Notifications() {
     const {selectedGroup} = useContext(PageContext)
     const query = useGroupNotificationsQuery(selectedGroup)
 
-    if (query.isLoading) return <></>
-    return <Content notifications={query.data} />
-}
-
-function Content(props: {notifications: GroupNotification[]}) {
-    const {notifications} = props
     const textColor = useColorModeValue('secondaryGray.900', 'white');
 
     return <Card p='0px'>
@@ -30,8 +24,15 @@ function Content(props: {notifications: GroupNotification[]}) {
             </Text>
             <Button variant='action'>Clear all</Button>
         </Flex>
-        {
-            notifications.map((n, i) => <GroupNotificationItem key={i} {...n} />)
+        {query.isLoading?
+            <>
+                <NotificationSkeleton />
+                <NotificationSkeleton />
+                <NotificationSkeleton />
+            </> :
+            query.data.map((n, i) =>
+                <GroupNotificationItem key={i} {...n} />
+            )
         }
     </Card>
 }

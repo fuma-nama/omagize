6c376dc3c18ca49a149fb437cb172bb2bd6eead1
SonMooSans <god63820869@gmail.com>
2022-09-11 18:18:32 +0800

    Add banner to home page

--- src/views/admin/default/index.tsx
+++ src/views/admin/default/index.tsx
@@ -1,25 +1,3 @@
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___   
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
- | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-                                                                                                                                                                                                                                                                                                                                       
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
-
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2022 Horizon UI (https://www.horizon-ui.com/)
-
-* Designed and Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 // Chakra imports
 import { Avatar, Box, Flex, FormLabel, Icon, Select, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
 // Assets
@@ -39,13 +17,20 @@ import WeeklyRevenue from 'views/admin/default/components/WeeklyRevenue';
 import { columnsDataCheck, columnsDataComplex } from 'views/admin/default/variables/columnsData';
 import tableDataCheck from 'views/admin/default/variables/tableDataCheck.json';
 import tableDataComplex from 'views/admin/default/variables/tableDataComplex.json';
+import Banner from "./components/Banner";
+import {useUserQuery} from "../../../api/UserAPI";
 
 export default function UserReports() {
 	// Chakra Color Mode
+	const query = useUserQuery()
 	const brandColor = useColorModeValue('brand.500', 'white');
 	const boxBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.100');
+
+	if (query.isLoading) return
 	return (
-		<Box>
+		<Flex direction='column' gap={5}>
+			<Banner user={query.data} />
+
 			<SimpleGrid columns={{ base: 1, md: 2, lg: 3, '2xl': 6 }} gap='20px' mb='20px'>
 				<MiniStatistics
 					startContent={
@@ -132,6 +117,6 @@ export default function UserReports() {
 					<MiniCalendar h='100%' minW='100%' selectRange={false} />
 				</SimpleGrid>
 			</SimpleGrid>
-		</Box>
+		</Flex>
 	);
 }

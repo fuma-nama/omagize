330acd9f08384de8ce9f2529e28db09657b96c4e
SonMooSans <god63820869@gmail.com>
2022-09-12 13:27:01 +0800

    Add Notifications Menu to Navbar

--- src/components/navbar/NavbarLinksAdmin.tsx
+++ src/components/navbar/NavbarLinksAdmin.tsx
@@ -1,44 +1,21 @@
 // Chakra Imports
 import {
-	Avatar,
-	Button,
 	Flex,
-	Icon,
-	Image,
-	Link,
-	Menu,
-	MenuButton,
-	MenuItem,
-	MenuList,
-	Text,
-	useColorModeValue,
-	useColorMode
 } from '@chakra-ui/react';
 // Custom Components
-import { ItemContent } from 'components/menu/ItemContent';
 import { SearchBar } from 'components/navbar/searchBar/SearchBar';
 import { SidebarResponsive } from 'components/sidebar/Sidebar';
-import PropTypes from 'prop-types';
 import React from 'react';
-// Assets
-
-import { MdNotificationsNone} from 'react-icons/md';
 
 import routes from 'routes';
-import {UserMenu} from "./profile/UserMenu";
+import {UserMenu} from "./menu/UserMenu";
 import ThemeSwitch from "./components/ThemeSwitch";
+import NotificationsMenu from "./menu/NotificationsMenu";
+import {useNavbarColors} from "../../variables/colors";
 
 export default function HeaderLinks() {
 	// Chakra Color Mode
-	const navbarIcon = useColorModeValue('gray.400', 'white');
-	let menuBg = useColorModeValue('white', 'navy.800');
-	const textColor = useColorModeValue('secondaryGray.900', 'white');
-	const textColorBrand = useColorModeValue('brand.700', 'brand.400');
-
-	const shadow = useColorModeValue(
-		'14px 17px 40px 4px rgba(112, 144, 176, 0.18)',
-		'14px 17px 40px 4px rgba(112, 144, 176, 0.06)'
-	);
+	const {iconColor, textColorPrimary, menuBg, shadow} = useNavbarColors()
 
 	return (
 		<Flex
@@ -55,40 +32,9 @@ export default function HeaderLinks() {
 				borderRadius='30px'
 			/>
 			<SidebarResponsive routes={routes} />
-			<Menu styleConfig={{
-				"min-width": "0"
-			}}>
-				<MenuButton p='0px'>
-					<Icon mt='6px' as={MdNotificationsNone} color={navbarIcon} w='18px' h='18px' me='10px' />
-				</MenuButton>
-				<MenuList
-					boxShadow={shadow}
-					p='20px'
-					borderRadius='20px'
-					bg={menuBg}
-					border='none'
-					w={{ base: '300px', md: '100%' }}>
-					<Flex w='100%' mb='20px'>
-						<Text fontSize='md' fontWeight='600' color={textColor}>
-							Notifications
-						</Text>
-						<Text fontSize='sm' fontWeight='500' color={textColorBrand} ms='auto' cursor='pointer'>
-							Mark all read
-						</Text>
-					</Flex>
-					<Flex flexDirection='column'>
-						<MenuItem _hover={{ bg: 'none' }} _focus={{ bg: 'none' }} px='0' borderRadius='8px' mb='10px'>
-							<ItemContent info='Horizon UI Dashboard PRO' />
-						</MenuItem>
-						<MenuItem _hover={{ bg: 'none' }} _focus={{ bg: 'none' }} px='0' borderRadius='8px' mb='10px'>
-							<ItemContent info='Horizon Design System Free' />
-						</MenuItem>
-					</Flex>
-				</MenuList>
-			</Menu>
-
-			<ThemeSwitch color={navbarIcon} />
-			<UserMenu color={textColor} shadow={shadow} bg={menuBg} />
+			<NotificationsMenu />
+			<ThemeSwitch color={iconColor} />
+			<UserMenu color={textColorPrimary} shadow={shadow} bg={menuBg} />
 		</Flex>
 	);
 }

75e50a91beb9147da7fae660ab0f759667a4928d
SonMooSans <god63820869@gmail.com>
2022-09-12 13:29:39 +0800

    Add colors

--- src/variables/colors.ts
+++ src/variables/colors.ts
@@ -14,6 +14,19 @@ export function useColors() {
         textColorPrimary: useColorModeValue('secondaryGray.900', 'white'),
         textColorSecondary: 'gray.400',
         borderColor: useColorModeValue('white !important', '#111C44 !important'),
-        cardBg: useColorModeValue('white', 'navy.800')
+        cardBg: useColorModeValue('white', 'navy.800'),
+        menuBg: useColorModeValue('white', 'navy.800'),
+        shadow: useColorModeValue(
+            '14px 17px 40px 4px rgba(112, 144, 176, 0.18)',
+            '14px 17px 40px 4px rgba(112, 144, 176, 0.06)'
+        )
+    }
+}
+
+export function useNavbarColors() {
+    return {
+        textColorBrand: useColorModeValue('brand.700', 'brand.400'),
+        iconColor: useColorModeValue('gray.400', 'white'),
+        ...useColors()
     }
 }

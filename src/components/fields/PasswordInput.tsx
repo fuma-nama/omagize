2b3d72b147bcb21dc7925976f5782d0ce80714bb
SonMooSans <god63820869@gmail.com>
2022-09-17 17:49:09 +0800

    Add Password Input Component

---
+++ src/components/fields/PasswordInput.tsx
@@ -0,0 +1,30 @@
+import {Icon, Input, InputGroup, InputGroupProps, InputProps, InputRightElement} from "@chakra-ui/react";
+import {RiEyeCloseLine} from "react-icons/ri";
+import {MdOutlineRemoveRedEye} from "react-icons/md";
+import React from "react";
+import {useColors} from "variables/colors";
+
+export default function PasswordInput(props: {input?: InputProps, group?: InputGroupProps }) {
+    const [show, setShow] = React.useState(false);
+    const {textColorSecondary} = useColors()
+
+    return <InputGroup size='md' {...props.group}>
+        <Input
+            isRequired={true}
+            fontSize='sm'
+            mb='24px'
+            size='lg'
+            type={show ? "text" : "password"}
+            variant='auth'
+            {...props.input}
+        />
+        <InputRightElement display='flex' alignItems='center' mt='4px'>
+            <Icon
+                color={textColorSecondary}
+                _hover={{ cursor: "pointer" }}
+                as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
+                onClick={() => setShow(!show)}
+            />
+        </InputRightElement>
+    </InputGroup>
+}

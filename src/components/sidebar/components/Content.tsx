ecc038fef2c42d484584f8fd3c34e53ae2cfcd66
SonMooSans <god63820869@gmail.com>
2022-10-08 22:20:31 +0800

    Handle fetch errors

--- src/components/sidebar/components/Content.tsx
+++ src/components/sidebar/components/Content.tsx
@@ -1,5 +1,5 @@
 // chakra imports
-import {Box, Flex, Stack} from '@chakra-ui/react';
+import {Box, Button, Center, Flex, Slide, Stack, Text, VStack} from '@chakra-ui/react';
 //   Custom components
 import Brand from 'components/sidebar/components/Brand';
 import Links from 'components/sidebar/components/Links';
@@ -9,6 +9,7 @@ import {ChatGroup, ChatGroupSkeleton} from "../../card/ChatGroup";
 import {useContext} from "react";
 import {PageContext} from "../../../contexts/PageContext";
 import ActionBar from "./ActionBar";
+import {SlideError} from "../../card/utils/SlideError";
 
 // FUNCTIONS
 
@@ -19,32 +20,36 @@ function SidebarContent(props: { routes: RoutesType[] }) {
 
 	// SIDEBAR
 	return (
-		<Flex direction='column' height='100%' pt='25px' borderRadius='30px' overflow='auto'>
-			<Brand />
-			<Stack direction='column' mt='18px' mb='auto'>
-				<Box ps='10px'>
-					<Links routes={routes} />
-				</Box>
-				<Box px='10px'>
-					<ActionBar />
-				</Box>
+		<>
+			<SlideError isError={query.isError} message='Failed to Fetch Groups' retry={() => query.refetch()} />
+			<Flex direction='column' height='100%' pt='25px' borderRadius='30px' overflow='auto'>
+				<Brand />
+				<Stack direction='column' mt='18px' mb='auto'>
+					<Box ps='10px'>
+						<Links routes={routes} />
+					</Box>
+					<Box px='10px'>
+						<ActionBar />
+					</Box>
 
-				<Flex direction='column' ps='10px' gap={3}>
-					{
-						query.isLoading?
-							<>
-								<ChatGroupSkeleton />
-								<ChatGroupSkeleton />
-							</>:
-							query.data.map(group => <ChatGroup key={group.id} group={group} active={selectedGroup === group.id} />)
-					}
-				</Flex>
-			</Stack>
+					<Flex direction='column' ps='10px' gap={3}>
+						{
+							query.isSuccess?
+								query.data?.map(group => <ChatGroup key={group.id} group={group} active={selectedGroup === group.id} />) :
+								<>
+									<ChatGroupSkeleton />
+									<ChatGroupSkeleton />
+								</>
+						}
+					</Flex>
+				</Stack>
 
-			<Box ps='20px' pe={{ lg: '16px', '2xl': '20px' }} mt='60px' mb='40px' borderRadius='30px'>
-				<SidebarCard />
-			</Box>
-		</Flex>
+				<Box ps='20px' pe={{ lg: '16px', '2xl': '20px' }} mt='60px' mb='40px' borderRadius='30px'>
+					<SidebarCard />
+				</Box>
+			</Flex>
+		</>
+
 	);
 }
 

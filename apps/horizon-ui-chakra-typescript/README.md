3d3a4394230b8c6494af05e20e5d0bb21675e377
SonMooSans <god63820869@gmail.com>
2022-10-11 23:56:42 +0800

    Migrate to NX

---
+++ apps/horizon-ui-chakra-typescript/README.md
@@ -0,0 +1,4 @@
+# In Development
+This project is a Modern Web Chat App built with React and Chakra UI
+<br>
+Based on Template: Horizon UI 
---
+++ apps/horizon-ui-chakra-typescript/src/layouts.tsx
@@ -0,0 +1,63 @@
+import AuthLayout from './layouts/auth';
+import AdminLayout from './layouts/admin';
+import { ReactNode } from 'react';
+import SidebarLayout from './layouts/SidebarLayout';
+import ChatLayout from './layouts/chat';
+import { Navigate } from 'react-router-dom';
+import React from 'react';
+
+export const layouts: RootLayout[] = [
+  {
+    type: 'auto',
+    path: '/auth',
+    component: <AuthLayout />,
+    index: '/auth/signin',
+    requireLogin: false,
+  },
+  {
+    type: 'normal',
+    path: '/user',
+    component: <SidebarLayout />,
+    subLayouts: [
+      {
+        index: true,
+        component: <Navigate to="/user/default" />,
+      },
+      {
+        path: 'chat',
+        component: <ChatLayout />,
+        routes: '/user/chat',
+      },
+      {
+        path: '*',
+        component: <AdminLayout />,
+        routes: '/user',
+      },
+    ],
+    requireLogin: true,
+  },
+];
+
+export type NestedLayout = {
+  index?: boolean;
+  path?: string;
+  component: ReactNode;
+  subLayouts?: NestedLayout[];
+  routes?: string;
+};
+
+export type AutoLayout = {
+  path: string;
+  component: ReactNode;
+  index?: string;
+  default?: string;
+  requireLogin: boolean;
+};
+
+export type RootLayout = LayoutType & {
+  requireLogin: boolean;
+};
+
+export type LayoutType =
+  | (AutoLayout & { type: 'auto' })
+  | (NestedLayout & { type: 'normal' });

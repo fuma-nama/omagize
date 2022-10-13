e3c39694f3a9cc5e3875abb8baf5f2b975dada39
SonMooSans <god63820869@gmail.com>
2022-10-13 19:33:42 +0800

    Improve Loading Screen

--- apps/web/src/components/screens/LoadingScreen.tsx
+++ apps/web/src/components/screens/LoadingScreen.tsx
@@ -1,10 +1,30 @@
-import { Center, Text, VStack } from '@chakra-ui/react';
+import { Box, Center, Icon, Spinner, Text, VStack } from '@chakra-ui/react';
+import { OmagizeLogo } from 'components/icons/Icons';
+import { useColors } from 'variables/colors';
 
 export default function LoadingScreen() {
+  const { brand } = useColors();
+
   return (
     <Center w="full" h="full">
       <VStack>
-        <Text>Loading</Text>
+        <Box pos="relative" p={5}>
+          <Spinner
+            pos="absolute"
+            top={0}
+            left={0}
+            w="full"
+            h="full"
+            thickness="4px"
+            color={brand}
+          />
+
+          <Icon color={brand} as={OmagizeLogo} w="100px" h="100px" />
+        </Box>
+
+        <Text color={brand} fontWeight="bold">
+          Loading
+        </Text>
       </VStack>
     </Center>
   );

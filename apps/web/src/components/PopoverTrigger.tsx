ed2b46f5a117a04f5339ffaaa664c3fad3bebb55
SonMooSans <god63820869@gmail.com>
2022-10-18 23:02:40 +0800

    Fix type errors of Popover

---
+++ apps/web/src/components/PopoverTrigger.tsx
@@ -0,0 +1,10 @@
+import {
+  PopoverTrigger as OrigPopoverTrigger,
+  PopoverAnchor as OrigPopoverAnchor,
+} from '@chakra-ui/react';
+
+export const PopoverTrigger: React.FC<{ children: React.ReactNode }> =
+  OrigPopoverTrigger;
+
+export const PopoverAnchor: React.FC<{ children: React.ReactNode }> =
+  OrigPopoverAnchor;

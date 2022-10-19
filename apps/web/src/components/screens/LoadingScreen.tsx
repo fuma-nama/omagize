d3884d68ea62100a7590ef1a58dc8d57b96be47f
SonMooSans <god63820869@gmail.com>
2022-10-19 21:55:27 +0800

    Add `QueryScreen` component

--- apps/web/src/components/screens/LoadingScreen.tsx
+++ apps/web/src/components/screens/LoadingScreen.tsx
@@ -1,6 +1,24 @@
 import { Box, Center, Icon, Spinner, Text, VStack } from '@chakra-ui/react';
+import { UseQueryResult } from '@tanstack/react-query';
 import { OmagizeLogo } from 'components/icons/Icons';
+import { ReactNode } from 'react';
 import { useColors } from 'variables/colors';
+import { ErrorScreen } from './ErrorScreen';
+
+export function QueryScreen({
+  query,
+  error,
+  children,
+}: {
+  query: UseQueryResult;
+  error: string;
+  children: ReactNode;
+}) {
+  if (query.isError)
+    return <ErrorScreen retry={() => query.refetch()}>{error}</ErrorScreen>;
+  if (query.isLoading) return <LoadingScreen />;
+  return <>{children}</>;
+}
 
 export default function LoadingScreen() {
   const { brand } = useColors();

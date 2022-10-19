fe742ff624ef416687877be2e4e4bfc2a5afdfa2
SonMooSans <god63820869@gmail.com>
2022-10-19 21:58:51 +0800

    Move `DateTimeForm` to Modal  Utils

--- apps/web/src/components/modals/Modal.tsx
+++ apps/web/src/components/modals/Modal.tsx
@@ -9,10 +9,14 @@ import {
   ButtonProps,
   Icon,
   Avatar,
+  SimpleGrid,
 } from '@chakra-ui/react';
 import ReactCrop, { Crop } from 'react-image-crop';
 import { FaImage } from 'react-icons/fa';
 import { MutableRefObject } from 'react';
+import { DatePicker } from 'components/picker/DatePicker';
+import { TimePicker } from 'components/picker/TimePicker';
+import { applyDate } from 'utils/DateUtils';
 
 export type CropImage = Crop | null;
 export type CropOptions = {
@@ -180,5 +184,38 @@ export function DynamicModal({
     <>
       <Fragment key={id}>{children}</Fragment>
     </>
+  );
+}
+
+export function DateTimeForm(props: {
+  min?: Date;
+  max?: Date;
+  value?: Date;
+  onChange: (date: Date) => void;
+}) {
+  return (
+    <SimpleGrid columns={{ base: 1, '2sm': 2 }} gap={4}>
+      <DatePicker
+        minDate={props.min}
+        maxDate={props.max}
+        value={props.value}
+        onChange={(date: Date) => {
+          const combined = new Date(date);
+          if (!!props.value) {
+            combined.setHours(props.value.getHours(), props.value.getMinutes());
+          }
+          props.onChange(combined);
+        }}
+      />
+      <TimePicker
+        value={
+          !!props.value && {
+            hours: props.value.getHours(),
+            minutes: props.value.getMinutes(),
+          }
+        }
+        onChange={(v) => props.onChange(applyDate(props.value, v))}
+      />
+    </SimpleGrid>
   );
 }

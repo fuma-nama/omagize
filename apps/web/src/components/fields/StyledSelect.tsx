86a246df2274eacada759bbf44c0620904bd8889
SonMooSans <god63820869@gmail.com>
2022-10-20 18:55:27 +0800

    `Select` Componet fix Dropdown Menu zIndex in Modal

--- apps/web/src/components/fields/StyledSelect.tsx
+++ apps/web/src/components/fields/StyledSelect.tsx
@@ -8,16 +8,26 @@ export default function StyledSelect<
   Option = unknown,
   IsMulti extends boolean = false,
   Group extends GroupBase<Option> = GroupBase<Option>
->(
-  props: Partial<
-    Props<Option, IsMulti, Group> &
-      RefAttributes<SelectInstance<Option, IsMulti, Group>>
-  >
-) {
+>({
+  portal,
+  ...props
+}: Partial<
+  Props<Option, IsMulti, Group> &
+    RefAttributes<SelectInstance<Option, IsMulti, Group>>
+> & {
+  portal?: boolean;
+}) {
   const { brand, cardBg } = useColors();
 
   return (
     <Select
+      styles={{
+        menuPortal: (provided) => ({
+          ...provided,
+          zIndex: '1500',
+        }),
+      }}
+      menuPortalTarget={portal && document.body}
       focusBorderColor={brand}
       chakraStyles={{
         option: (provided, state) => ({

495924e0f0c5145330aa574cc1ab66cd6b7e4da2
SonMooSans <god63820869@gmail.com>
2022-11-01 00:26:34 +0800

    Add Firebase Cloud Messaging API

--- libs/api/src/firebase/firebase.ts
+++ libs/api/src/firebase/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { FirebaseOptions, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getMessaging } from 'firebase/messaging';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +20,6 @@ const firebaseConfig: FirebaseOptions = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const messaging = getMessaging(app);
 
-export const firebase = { app, auth };
+export const firebase = { app, auth, messaging };
---
+++ libs/api/src/firebase/messaging.ts
@@ -0,0 +1,25 @@
+import { getToken } from 'firebase/messaging';
+import { firebase } from './firebase';
+
+const token = getToken(firebase.messaging, {
+  vapidKey:
+    'BCi2YcfIWvkPn67wWyMDok6HW1qEpaKSyjkBsqfhVL-vXgctPeGBlnkTtxQjuP-DUg1sZZoxk8rWHJ_nu3b0RWI',
+});
+
+export class FirebaseMessaging {
+  /**
+   * @returns true if success
+   */
+  static async requestPermission(): Promise<boolean> {
+    console.log('Requesting permission...');
+    const permission = await Notification.requestPermission();
+    const success = permission === 'granted';
+
+    if (success) {
+      console.log('Notification permission granted.');
+    } else {
+      console.log('denied');
+    }
+    return success;
+  }
+}

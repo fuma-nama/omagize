67ef2458aa801ae405901be6344d7272f41d59d2
SonMooSans <god63820869@gmail.com>
2022-11-02 16:54:20 +0800

    Update Gateway Event Codes

---
+++ libs/api/src/gateway/events.ts
@@ -0,0 +1,37 @@
+import { RawGroup } from '../GroupAPI';
+export type GatewayEvent<T> = {
+  op: OpCode;
+  d: T;
+  type?: EventType;
+};
+
+export enum OpCode {
+  Identity = 0,
+  Ready = 1,
+  Dispatch = 3,
+}
+
+export enum EventType {
+  GroupAdded = 'group_added',
+  GroupRemoved = 'group_removed',
+  GroupUpdated = 'group_updated',
+  GroupEventCreated = 'group_event_created',
+  UserUpdated = 'user_updated',
+  MessageCreated = 'message_created',
+}
+
+type IdentityMessage = {
+  token: string;
+};
+export function IdentityEvent(token: string): GatewayEvent<IdentityMessage> {
+  return {
+    op: OpCode.Identity,
+    d: {
+      token,
+    },
+  };
+}
+
+export type GroupAddedEvent = {
+  group: RawGroup;
+};

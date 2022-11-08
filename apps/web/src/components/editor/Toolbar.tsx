dba9a2d11d4f8b58e94bd2e7c03a68d2f8411f1a
SonMooSans <god63820869@gmail.com>
2022-11-08 21:13:40 +0800

    Fix Toolbar overflow on mobile

--- apps/web/src/components/editor/Toolbar.tsx
+++ apps/web/src/components/editor/Toolbar.tsx
@@ -1,9 +1,43 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
-import { ReactElement } from 'react';
+import { Button, ButtonGroup, Hide, IconButton, Show } from '@chakra-ui/react';
+import { Fragment, ReactElement } from 'react';
 import { ValueProps } from 'components/editor/MessageInput';
 import { DraftInlineStyleType, RichUtils } from 'draft-js';
 import { useColors } from 'variables/colors';
-import { BiCode, BiItalic, BiStrikethrough, BiUnderline } from 'react-icons/bi';
+import {
+  BiBold,
+  BiCode,
+  BiItalic,
+  BiStrikethrough,
+  BiUnderline,
+} from 'react-icons/bi';
+
+const styles: Array<{
+  style: DraftInlineStyleType;
+  label: string;
+  icon?: ReactElement;
+}> = [
+  {
+    style: 'BOLD',
+    label: 'Bold',
+    icon: <BiBold />,
+  },
+  { style: 'ITALIC', label: 'Italic', icon: <BiItalic /> },
+  {
+    style: 'CODE',
+    label: 'Code',
+    icon: <BiCode />,
+  },
+  {
+    style: 'STRIKETHROUGH',
+    label: 'Strike Through',
+    icon: <BiStrikethrough />,
+  },
+  {
+    style: 'UNDERLINE',
+    label: 'Underline',
+    icon: <BiUnderline />,
+  },
+];
 
 export function Toolbar({ value, onChange }: ValueProps) {
   const style = value.getCurrentInlineStyle();
@@ -12,53 +46,39 @@ export function Toolbar({ value, onChange }: ValueProps) {
     onChange(RichUtils.toggleInlineStyle(value, style));
   };
 
-  const styles: Array<{
-    style: DraftInlineStyleType;
-    label: string;
-    icon?: ReactElement;
-  }> = [
-    {
-      style: 'BOLD',
-      label: 'Bold',
-    },
-    { style: 'ITALIC', label: 'Italic', icon: <BiItalic /> },
-    {
-      style: 'CODE',
-      label: 'Code',
-      icon: <BiCode />,
-    },
-    {
-      style: 'STRIKETHROUGH',
-      label: 'Strike Through',
-      icon: <BiStrikethrough />,
-    },
-    {
-      style: 'UNDERLINE',
-      label: 'Underline',
-      icon: <BiUnderline />,
-    },
-  ];
-
   return (
-    <ButtonGroup bg={globalBg}>
+    <ButtonGroup w="full" bg={globalBg}>
       {styles.map((s) => {
         const checked = style?.contains(s.style);
+        const onMouseDown = (e: any) => {
+          e.preventDefault();
+          toggleStyle(s.style);
+        };
 
         return (
-          <Button
-            key={s.style}
-            onMouseDown={(e) => {
-              e.preventDefault();
-              toggleStyle(s.style);
-            }}
-            color={checked && 'white'}
-            bg={checked && brand}
-            variant="outline"
-            _hover={{}}
-            leftIcon={s.icon}
-          >
-            {s.label}
-          </Button>
+          <Fragment key={s.style}>
+            <Show above="md">
+              <Button
+                onMouseDown={onMouseDown}
+                color={checked && 'white'}
+                bg={checked && brand}
+                _hover={{}}
+                leftIcon={s.icon}
+              >
+                {s.label}
+              </Button>
+            </Show>
+            <Hide above="md">
+              <IconButton
+                aria-label={s.style}
+                onMouseDown={onMouseDown}
+                color={checked && 'white'}
+                bg={checked && brand}
+                icon={s.icon}
+                _hover={{}}
+              />
+            </Hide>
+          </Fragment>
         );
       })}
     </ButtonGroup>

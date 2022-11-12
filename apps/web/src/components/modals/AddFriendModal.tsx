9e2250934ed815afba16b72e41b24b055d7b6d3d
SonMooSans <god63820869@gmail.com>
2022-11-12 18:49:19 +0800

    Rewrite add friend Modal

--- apps/web/src/components/modals/AddFriendModal.tsx
+++ apps/web/src/components/modals/AddFriendModal.tsx
@@ -1,10 +1,5 @@
 import {
   Button,
-  FormControl,
-  FormErrorMessage,
-  FormHelperText,
-  FormLabel,
-  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -12,13 +7,21 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
   Text,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { BiRightArrow } from 'react-icons/bi';
 import { useMutation } from '@tanstack/react-query';
-import { sendFriendRequest } from '@omagize/api';
-import { parseError } from 'utils/APIUtils';
+import { sendFriendRequest, User } from '@omagize/api';
+import {
+  SearchUserIDPanel,
+  SearchUserNamePanel,
+} from '../panel/SearchUserPanel';
+import { TabButton } from 'components/layout/Tab';
 
 export default function AddFriendModal({
   isOpen,
@@ -27,11 +30,9 @@ export default function AddFriendModal({
   isOpen: boolean;
   onClose: () => void;
 }) {
-  const [id, setId] = useState('');
-  const mutation = useMutation(['send_friend_request', id], () =>
-    sendFriendRequest(id)
-  );
-  const disable = id.length === 0 || mutation.isLoading;
+  const [selected, setSelected] = useState<User>();
+  const mutation = useMutation(() => sendFriendRequest(selected.id));
+  const disable = selected == null || mutation.isLoading;
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
@@ -40,20 +41,27 @@ export default function AddFriendModal({
         <ModalHeader>Add a Friend</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Text mb={5}>You can find the Friend ID from user profile</Text>
-          <FormControl isRequired isInvalid={mutation.isError}>
-            <FormLabel>Friend ID</FormLabel>
-            <Input
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-              variant="main"
-              placeholder="XXXXXX"
-            />
-            <FormErrorMessage>{parseError(mutation.error)}</FormErrorMessage>
-            <FormHelperText hidden={mutation.isError}>
-              Friend ID is a 6-length number
-            </FormHelperText>
-          </FormControl>
+          <Text mb={5}>You can search the User</Text>
+          <Tabs variant="soft-rounded" minH="200px">
+            <TabList>
+              <TabButton>By Name</TabButton>
+              <TabButton>User ID</TabButton>
+            </TabList>
+            <TabPanels>
+              <TabPanel px={0}>
+                <SearchUserNamePanel
+                  value={selected}
+                  onChange={(e) => setSelected(e)}
+                />
+              </TabPanel>
+              <TabPanel px={0}>
+                <SearchUserIDPanel
+                  value={selected}
+                  onChange={(e) => setSelected(e)}
+                />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </ModalBody>
 
         <ModalFooter>

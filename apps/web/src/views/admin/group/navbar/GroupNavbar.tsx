db2e5384e6c23bbe332add8cd007552d18070fd6
SonMooSans <god63820869@gmail.com>
2022-11-15 18:46:13 +0800

    Improve Group navbar

--- apps/web/src/views/admin/group/navbar/GroupNavbar.tsx
+++ apps/web/src/views/admin/group/navbar/GroupNavbar.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, HStack, useColorModeValue } from '@chakra-ui/react';
+import { Avatar, Box, Button, HStack, Text } from '@chakra-ui/react';
 import { SearchBar } from 'components/navbar/searchBar/SearchBar';
 import { SidebarResponsive } from 'components/sidebar/Sidebar';
 import items from 'sidebar';
@@ -6,45 +6,54 @@ import ThemeSwitch from 'components/navbar/components/ThemeSwitch';
 import { UserMenu } from 'components/navbar/menu/UserMenu';
 import { BiArrowBack } from 'react-icons/bi';
 import { useSelected } from 'utils/navigate';
+import { NavbarLinksBox } from 'components/navbar/NavbarLinksAdmin';
+import { NavbarBox } from 'components/navbar/NavbarAdmin';
+import { useGroup } from 'stores/hooks';
+import { useNavbarColors } from 'variables/colors';
 
-export default function GroupNavbar() {
-  // Chakra Color Mode
-  const navbarIcon = useColorModeValue('gray.400', 'white');
-  let menuBg = useColorModeValue('white', 'navy.800');
-  const textColor = useColorModeValue('secondaryGray.900', 'white');
+export function GroupNavbar({ isRoot }: { isRoot?: boolean }) {
+  const { selectedGroup } = useSelected();
+  const group = useGroup(selectedGroup);
 
-  const shadow = useColorModeValue(
-    '14px 17px 40px 4px rgba(112, 144, 176, 0.18)',
-    '14px 17px 40px 4px rgba(112, 144, 176, 0.06)'
+  return (
+    <NavbarBox>
+      <HStack>
+        <Avatar src={group?.iconUrl} name={group?.name} />
+        <Text fontWeight="600" fontSize="xl">
+          {group?.name}
+        </Text>
+      </HStack>
+      <Box ms="auto" w={{ sm: '100%', md: 'unset' }}>
+        <GroupNavbarLinks isRoot={isRoot} />
+      </Box>
+    </NavbarBox>
   );
+}
+
+export default function GroupNavbarLinks({ isRoot }: { isRoot?: boolean }) {
+  const { iconColor, textColorPrimary, menuBg, shadow } = useNavbarColors();
   const { selectedGroup, setSelectedGroup } = useSelected();
 
   return (
-    <Flex
-      w={{ base: '100%', md: 'auto' }}
-      direction="column"
-      gap={2}
-      bg={menuBg}
-      p="10px"
-      borderRadius="30px"
-      boxShadow={shadow}
-    >
-      <HStack ml={2}>
-        <Button
-          leftIcon={<BiArrowBack />}
-          onClick={() => setSelectedGroup(selectedGroup)}
-          variant="link"
-        >
-          Back
-        </Button>
-      </HStack>
-      <HStack align="center">
-        <SearchBar mb="unset" me="10px" />
+    <NavbarLinksBox alignItems="start" direction="column">
+      {!isRoot && (
+        <HStack ml={2}>
+          <Button
+            leftIcon={<BiArrowBack />}
+            onClick={() => setSelectedGroup(selectedGroup)}
+            variant="link"
+          >
+            Back
+          </Button>
+        </HStack>
+      )}
+      <HStack w="full" align="center">
+        <SearchBar mb="unset" me="10px" w="full" />
         <SidebarResponsive items={items} />
 
-        <ThemeSwitch color={navbarIcon} />
-        <UserMenu color={textColor} shadow={shadow} bg={menuBg} />
+        <ThemeSwitch color={iconColor} />
+        <UserMenu color={textColorPrimary} shadow={shadow} bg={menuBg} />
       </HStack>
-    </Flex>
+    </NavbarLinksBox>
   );
 }

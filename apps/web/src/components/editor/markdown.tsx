708043cf9a0ac23c496767b3d3a386ceeaba7a17
SonMooSans <god63820869@gmail.com>
2022-11-20 19:55:02 +0800

    Improve markdown serialization

--- apps/web/src/components/editor/markdown.tsx
+++ apps/web/src/components/editor/markdown.tsx
@@ -1,6 +1,7 @@
 import { Mention } from '@omagize/api';
 import { Editor, Element, Node, Text } from 'slate';
 import { toAPIMentionType } from 'utils/markdown/types';
+import { TextMarks } from './types';
 
 export type Parsed = Entities & {
   markdown: string;
@@ -15,11 +16,27 @@ export function slateToMarkdown(editor: Editor, entities: Entities = { mentions:
   };
 }
 
-function nodeToMarkdown(node: Node, entities: Entities): string {
+const convert: {
+  [key in keyof TextMarks]: string;
+} = {
+  italic: '*',
+  bold: '**',
+  code: '`',
+  strikethrough: '~~',
+  underlined: '__',
+};
+
+function nodeToMarkdown(
+  node: Node,
+  entities: Entities,
+  level: number = 0,
+  styles: Set<keyof TextMarks> = new Set()
+): string {
   if (Editor.isEditor(node)) return slateToMarkdown(node, entities).markdown;
 
   if (Text.isText(node)) {
-    let string = node.text;
+    let string = textToMarkdown(node, styles);
+
     //important
     if (node.blockquote) {
       string = `> ${string}`;
@@ -28,27 +45,43 @@ function nodeToMarkdown(node: Node, entities: Entities): string {
       string = `# ${string}`;
     }
 
-    if (node.italic) {
-      string = `*${string}*`;
-    }
-    if (node.bold) {
-      string = `**${string}**`;
-    }
-    if (node.code) {
-      string = `\`${string}\``;
-    }
-    if (node.strikethrough) {
-      string = `~~${string}~~`;
-    }
-    if (node.underlined) {
-      string = `__${string}__`;
-    }
-
     return string;
   }
 
-  const children = node.children.map((n) => nodeToMarkdown(n, entities)).join('');
+  let children = node.children.map((n) => nodeToMarkdown(n, entities, level + 1, styles)).join('');
 
+  for (const style of [...styles.values()].reverse()) {
+    children += convert[style];
+  }
+
+  return elementToMarkdown(node, children, entities);
+}
+
+export function textToMarkdown(node: Text, styles: Set<keyof TextMarks>): string {
+  let string = '';
+
+  for (const key of Object.keys(convert)) {
+    const style = key as keyof TextMarks;
+
+    if (node[style] && !styles.has(style)) {
+      string += convert[style];
+      styles.add(style);
+    }
+  }
+
+  string += node.text;
+
+  for (const style of [...styles.values()].reverse()) {
+    if (node[style] !== true) {
+      string += convert[style];
+      styles.delete(style);
+    }
+  }
+
+  return string;
+}
+
+export function elementToMarkdown(node: Element, children: string, entities: Entities) {
   switch (node.type) {
     case 'mention': {
       let type = toAPIMentionType(node.mention_type);

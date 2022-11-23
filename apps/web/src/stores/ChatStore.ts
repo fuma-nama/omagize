c1d09299390749bc148df1e696f85b4ac94d158b
SonMooSans <god63820869@gmail.com>
2022-11-23 19:38:14 +0800

    Create Sticker Items

--- apps/web/src/stores/ChatStore.ts
+++ apps/web/src/stores/ChatStore.ts
@@ -1,14 +1,18 @@
-import { CustomEmoji, Snowflake } from '@omagize/api';
+import { CustomSticker, CustomEmoji, Snowflake } from '@omagize/api';
 import create from 'zustand';
 
 export type ChatStore = {
   liked_emojis: CustomEmoji[] | null;
   likeEmoji: (emoji: CustomEmoji) => void;
   unlikeEmoji: (emoji: Snowflake) => void;
+  liked_stickers: CustomSticker[] | null;
+  likeSticker: (sticker: CustomSticker) => void;
+  unlikeSticker: (sticker: Snowflake) => void;
 };
 
 export const useChatStore = create<ChatStore>((set, get) => ({
   liked_emojis: [],
+  liked_stickers: [],
   likeEmoji: (emoji) =>
     set((store) => {
       const prev = store.liked_emojis;
@@ -24,6 +28,23 @@ export const useChatStore = create<ChatStore>((set, get) => ({
 
       return {
         liked_emojis: prev.filter((e) => e.id !== emoji),
+      };
+    }),
+  likeSticker: (sticker) =>
+    set((store) => {
+      const prev = store.liked_stickers;
+      const contains = prev.some((e) => e.id === sticker.id);
+
+      return {
+        liked_stickers: contains ? prev : [...prev, sticker],
+      };
+    }),
+  unlikeSticker: (sticker) =>
+    set((store) => {
+      const prev = store.liked_stickers;
+
+      return {
+        liked_stickers: prev.filter((e) => e.id !== sticker),
       };
     }),
 }));
---
+++ apps/web/src/views/admin/marketplace/components/StickerItem.tsx
@@ -0,0 +1,131 @@
+// Chakra imports
+import { Avatar, Box, Button, Center, Flex, Icon, IconButton, Image, Text } from '@chakra-ui/react';
+import { CustomSticker, likeAsset, unlikeAsset } from '@omagize/api';
+import { useMutation } from '@tanstack/react-query';
+// Custom components
+import Card from 'components/card/Card';
+// Assets
+import { IoHeart, IoHeartOutline } from 'react-icons/io5';
+import { useChatStore } from 'stores/ChatStore';
+import { useColorsExtend } from 'variables/colors';
+
+export default function StickerItem({ sticker }: { sticker: CustomSticker }) {
+  const { id, name, url, author } = sticker;
+  const store = useChatStore((s) => ({
+    hasLike: s.liked_stickers?.some((e) => e.id === id),
+    like: s.likeSticker,
+    unlike: s.unlikeSticker,
+  }));
+  const { brand, textColor, textColorBid } = useColorsExtend(
+    {
+      textColor: 'navy.700',
+      textColorBid: 'brand.500',
+    },
+    {
+      textColor: 'white',
+      textColorBid: 'white',
+    }
+  );
+
+  const likeMutation = useMutation(
+    (like: boolean) => (like ? likeAsset(id, 'sticker') : unlikeAsset(id, 'sticker')),
+    {
+      onSuccess: (_, _like) => {
+        _like ? store.like(sticker) : store.unlike(id);
+      },
+    }
+  );
+
+  return (
+    <Card p="20px">
+      <Flex direction={{ base: 'column' }} justify="center">
+        <Box mb={{ base: '20px', '2xl': '20px' }} position="relative">
+          <Center pos="relative" w="full" h="150px" rounded="md" bg={brand}>
+            <Image
+              top={0}
+              left={0}
+              pos="absolute"
+              src={url}
+              w="full"
+              h="full"
+              objectFit="cover"
+              filter="blur(8px)"
+              opacity={0.3}
+            />
+            <Image src={url} w="100px" h="100px" pos="relative" />
+          </Center>
+
+          <IconButton
+            aria-label="Like"
+            position="absolute"
+            bg="white"
+            top="7px"
+            right="7px"
+            _hover={{ bg: 'whiteAlpha.900' }}
+            _active={{ bg: 'whiteAlpha.900' }}
+            borderRadius="50%"
+            w="36px"
+            h="36px"
+            color="brand.500"
+            onClick={() => likeMutation.mutate(!store.hasLike)}
+            icon={<Icon w="20px" h="20px" as={store.hasLike ? IoHeart : IoHeartOutline} />}
+          />
+        </Box>
+        <Flex flexDirection="column" justify="space-between" h="100%">
+          <Flex justify="space-between" direction="row" mb="auto">
+            <Flex direction="column">
+              <Text
+                color={textColor}
+                fontSize={{
+                  base: 'xl',
+                  xl: 'lg',
+                  '2xl': 'md',
+                  '3xl': 'lg',
+                }}
+                mb="5px"
+                fontWeight="bold"
+                me="14px"
+              >
+                {name}
+              </Text>
+              <Text
+                color="secondaryGray.600"
+                fontSize={{
+                  base: 'sm',
+                }}
+                fontWeight="400"
+                me="14px"
+              >
+                {author.username}
+              </Text>
+            </Flex>
+            <Avatar src={author.avatarUrl} name={author.username} size="sm" />
+          </Flex>
+          <Flex
+            align="center"
+            justify="space-between"
+            direction="row"
+            mt="25px"
+            flexWrap="wrap"
+            gap={2}
+          >
+            <Text fontWeight="700" fontSize="sm" color={textColorBid}>
+              Likes: 0
+            </Text>
+            <Button
+              variant="darkBrand"
+              color="white"
+              fontSize="sm"
+              fontWeight="500"
+              borderRadius="70px"
+              px="24px"
+              py="5px"
+            >
+              View Sticker
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Card>
+  );
+}

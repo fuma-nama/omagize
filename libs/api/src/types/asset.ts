5cae103209ae553af51d7870fd50c81ede476fad
SonMooSans <god63820869@gmail.com>
2022-11-23 17:11:26 +0800

    Parse Asset image url

--- libs/api/src/types/asset.ts
+++ libs/api/src/types/asset.ts
@@ -1,47 +1,43 @@
 import { RawUser } from '../UserAPI';
+import { AssetType, getAssetUrl } from '../utils';
 import { DateObject, Snowflake } from './common';
 import { User } from './user';
 
-export type RawCustomEmoji = {
+type RawAsset = {
   id: Snowflake;
   name: string;
   author: RawUser;
   createdAt: DateObject;
 };
 
-export type CustomEmoji = {
+type Asset = {
   id: Snowflake;
   name: string;
   author: User;
   createdAt: Date;
+  url: string;
 };
+
+export type RawCustomEmoji = RawAsset;
+export type CustomEmoji = Asset;
 
 export function CustomEmoji(raw: RawCustomEmoji): CustomEmoji {
   return {
     ...raw,
     author: new User(raw.author),
     createdAt: new Date(raw.createdAt),
+    url: getAssetUrl(AssetType.Emojis, raw.id),
   };
 }
 
-export type RawCustomSticker = {
-  id: Snowflake;
-  name: string;
-  author: RawUser;
-  createdAt: DateObject;
-};
-
-export type CustomSticker = {
-  id: Snowflake;
-  name: string;
-  author: User;
-  createdAt: Date;
-};
+export type RawCustomSticker = RawAsset;
+export type CustomSticker = Asset;
 
 export function CustomSticker(raw: RawCustomSticker): CustomSticker {
   return {
     ...raw,
     author: new User(raw.author),
     createdAt: new Date(raw.createdAt),
+    url: getAssetUrl(AssetType.Stickers, raw.id),
   };
 }
--- libs/api/src/utils/mediaUtils.ts
+++ libs/api/src/utils/mediaUtils.ts
@@ -8,40 +8,32 @@ enum MediaType {
   Attachments = 'attachments',
 }
 
-export function toAvatarUrl(
-  user: Snowflake,
-  hash?: string | number
-): string | null {
+export enum AssetType {
+  Emojis = 'emojis',
+  Stickers = 'stickers',
+}
+
+export function toAvatarUrl(user: Snowflake, hash?: string | number): string | null {
   return asWebpUrl(MediaType.Avatars, user, hash);
 }
 
-export function toIconUrl(
-  group: Snowflake,
-  hash?: string | number
-): string | null {
+export function toIconUrl(group: Snowflake, hash?: string | number): string | null {
   return asWebpUrl(MediaType.Icons, group, hash);
 }
 
-export function toBannerUrl(
-  userOrGroup: Snowflake,
-  hash?: string | number
-): string | null {
+export function toBannerUrl(userOrGroup: Snowflake, hash?: string | number): string | null {
   return asWebpUrl(MediaType.Banner, userOrGroup, hash);
 }
 
-export function toAttachmentUrl(
-  message: Snowflake,
-  id: Snowflake,
-  name: string
-): string | null {
+export function getAssetUrl(type: AssetType, id: Snowflake): string {
+  return `${api}/media/${type}/${id}.webp`;
+}
+
+export function toAttachmentUrl(message: Snowflake, id: Snowflake, name: string): string | null {
   return `${api}/media/${MediaType.Attachments}/${message}/${id}/${name}`;
 }
 
-export function asUrl(
-  type: MediaType,
-  id: Snowflake,
-  hash?: string | number
-): string | null {
+export function asUrl(type: MediaType, id: Snowflake, hash?: string | number): string | null {
   if (hash != null) {
     return `${api}/media/${type}/${id}/${hash}`;
   } else {
@@ -49,11 +41,7 @@ export function asUrl(
   }
 }
 
-export function asWebpUrl(
-  type: MediaType,
-  id: Snowflake,
-  hash?: string | number
-): string | null {
+export function asWebpUrl(type: MediaType, id: Snowflake, hash?: string | number): string | null {
   if (hash != null) {
     return `${api}/media/${type}/${id}/${hash}.webp`;
   } else {

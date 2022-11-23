160eb6a21826337034f7e2d5a99cece2688a7062
SonMooSans <god63820869@gmail.com>
2022-11-23 18:57:46 +0800

    Create `QueryStatusLayout` component

--- apps/web/src/components/panel/QueryPanel.tsx
+++ apps/web/src/components/panel/QueryPanel.tsx
@@ -1,5 +1,5 @@
 import { UseQueryResult } from '@tanstack/react-query';
-import { ReactNode } from 'react';
+import { ReactElement, ReactNode } from 'react';
 import { ErrorPanel } from './ErrorPanel';
 import LoadingPanel, { Props } from './LoadingPanel';
 import { Center, VStack, Icon, Button, Text } from '@chakra-ui/react';
@@ -19,6 +19,31 @@ export function QueryStatus({
   if (query.isError) return <ErrorPanel retry={() => query.refetch()}>{error}</ErrorPanel>;
   if (query.isLoading) return <>{skeleton}</>;
   if (query.isSuccess) return <>{children}</>;
+
+  return <></>;
+}
+
+export function QueryStatusLayout({
+  watch,
+  query,
+  error,
+  placeholder,
+  skeleton,
+  children,
+  container = (s) => <>{s}</>,
+}: {
+  watch?: any[] | null;
+  query: UseQueryResult;
+  error: string;
+  placeholder: ReactElement;
+  skeleton: ReactNode;
+  children: ReactNode;
+  container: (c: ReactNode) => ReactElement;
+}) {
+  if (watch?.length === 0) return placeholder;
+  if (query.isError) return <ErrorPanel retry={() => query.refetch()}>{error}</ErrorPanel>;
+  if (query.isLoading) return container(skeleton);
+  if (query.isSuccess) return container(children);
 
   return <></>;
 }

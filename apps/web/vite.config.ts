a252665065dcddef43d2c66d76c8a32a627a0138
SonMooSans <god63820869@gmail.com>
2022-11-27 17:30:51 +0800

    Fix peer dependencies issues

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,7 +4,16 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+  ],
   server: {
     host: 'localhost',
     port: 3000,

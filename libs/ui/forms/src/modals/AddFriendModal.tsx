f0db53d696dd98ea8c68250fba3c6909fd53f07d
SonMooSans <god63820869@gmail.com>
2022-12-03 14:02:30 +0800

    Clean code

--- libs/ui/forms/src/modals/AddFriendModal.tsx
+++ libs/ui/forms/src/modals/AddFriendModal.tsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   Button,
   Flex,
   FormControl,
@@ -8,16 +7,11 @@ import {
   IconButton,
   Input,
   Modal,
-  ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
   Text,
   useModalContext,
 } from '@chakra-ui/react';
@@ -25,26 +19,8 @@ import { useState } from 'react';
 import { BiSend } from 'react-icons/bi';
 import { useMutation } from '@tanstack/react-query';
 import { parseError, sendFriendRequest, User, useSelfUser } from '@omagize/api';
-import {
-  useSearchUsernameQuery,
-  useSearchUserIdQuery,
-  ResultPlaceholder,
-  SelectedUserItem,
-} from '../panel/SearchUserPanel';
 import { useUserStore } from '@omagize/data-access-store';
-import { useColors } from '@omagize/ui/theme';
-import {
-  SearchBar,
-  TabButton,
-  QueryStatusLayout,
-  LoadingPanel,
-  QueryStatus,
-} from '@omagize/ui/components';
-
-enum Tab {
-  ByName = 0,
-  ById = 1,
-}
+import { SearchPanel } from '../panel/SearchUserPanel';
 
 function useCheckSelected(selected: User | null): string | null {
   const self = useSelfUser();
@@ -94,104 +70,6 @@ export function AddFriendModal({
         </ModalFooter>
       </ModalContent>
     </Modal>
-  );
-}
-
-function SearchPanel({
-  value,
-  onChange,
-  invalid,
-}: {
-  value: User;
-  onChange: (user: User) => void;
-  invalid?: boolean;
-}) {
-  const [tab, setTab] = useState<Tab>(0);
-  const [search, setSearch] = useState('');
-
-  const nameQuery = useSearchUsernameQuery();
-  const idQuery = useSearchUserIdQuery();
-
-  const { cardBg } = useColors();
-
-  const onSearch = () => {
-    if (search.trim().length === 0) return;
-
-    switch (tab) {
-      case Tab.ByName: {
-        nameQuery.setSearch(search);
-        break;
-      }
-      case Tab.ById: {
-        idQuery.setSearch(search);
-        break;
-      }
-    }
-  };
-
-  const userItem = (user: User) => (
-    <SelectedUserItem
-      key={user.id}
-      user={user}
-      selected={user.id === value?.id}
-      onClick={() => onChange(user)}
-      invalid={invalid}
-    />
-  );
-
-  return (
-    <>
-      <Box w="full" px={4}>
-        <SearchBar
-          w="full"
-          onSearch={onSearch}
-          input={{
-            bg: cardBg,
-            value: search,
-            type: 'search',
-            onChange: (e) => setSearch(e.target.value),
-          }}
-        />
-      </Box>
-      <ModalBody>
-        <Tabs
-          variant="soft-rounded"
-          index={tab}
-          onChange={setTab}
-          display="flex"
-          flexDirection="column"
-        >
-          <TabList>
-            <TabButton>By Name</TabButton>
-            <TabButton>User ID</TabButton>
-          </TabList>
-
-          <TabPanels overflow="auto" minH="100px">
-            <TabPanel px={0}>
-              <QueryStatusLayout
-                query={nameQuery.query}
-                watch={nameQuery.query.data}
-                placeholder={<ResultPlaceholder />}
-                loading={nameQuery.query.isInitialLoading && <LoadingPanel size="sm" />}
-                container={(c) => <Flex direction="column" children={c} />}
-                error="Failed to search users"
-              >
-                {nameQuery.query.data?.map((user) => userItem(user))}
-              </QueryStatusLayout>
-            </TabPanel>
-            <TabPanel px={0}>
-              <QueryStatus
-                query={idQuery.query}
-                loading={idQuery.query.isInitialLoading && <LoadingPanel size="sm" />}
-                error="Failed to search user"
-              >
-                {idQuery.query.data && userItem(idQuery.query.data)}
-              </QueryStatus>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </ModalBody>
-    </>
   );
 }
 
--- libs/ui/forms/src/panel/SearchUserPanel.tsx
+++ libs/ui/forms/src/panel/SearchUserPanel.tsx
@@ -1,14 +1,134 @@
 import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { fetchUserInfo, Keys, searchUser, User } from '@omagize/api';
-import { Flex, FlexProps, Icon } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  FlexProps,
+  Icon,
+  ModalBody,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from '@chakra-ui/react';
 import { useColorModeValue } from '@chakra-ui/system';
 import { BiCheckCircle, BiErrorCircle, BiSearch } from 'react-icons/bi';
 import { useColors } from '@omagize/ui/theme';
 import { SmallUserItem } from '@omagize/ui/items';
 import { Placeholder } from '@omagize/ui/components';
+import {
+  SearchBar,
+  TabButton,
+  QueryStatusLayout,
+  LoadingPanel,
+  QueryStatus,
+} from '@omagize/ui/components';
 
-export function useSearchUsernameQuery() {
+export enum Tab {
+  ByName = 0,
+  ById = 1,
+}
+
+export function SearchPanel({
+  value,
+  onChange,
+  invalid,
+}: {
+  value: User;
+  onChange: (user: User) => void;
+  invalid?: boolean;
+}) {
+  const [tab, setTab] = useState<Tab>(0);
+  const [search, setSearch] = useState('');
+
+  const nameQuery = useSearchUsernameQuery();
+  const idQuery = useSearchUserIdQuery();
+
+  const { cardBg } = useColors();
+
+  const onSearch = () => {
+    if (search.trim().length === 0) return;
+
+    switch (tab) {
+      case Tab.ByName: {
+        nameQuery.setSearch(search);
+        break;
+      }
+      case Tab.ById: {
+        idQuery.setSearch(search);
+        break;
+      }
+    }
+  };
+
+  const userItem = (user: User) => (
+    <SelectedUserItem
+      key={user.id}
+      user={user}
+      selected={user.id === value?.id}
+      onClick={() => onChange(user)}
+      invalid={invalid}
+    />
+  );
+
+  return (
+    <>
+      <Box w="full" px={4}>
+        <SearchBar
+          w="full"
+          onSearch={onSearch}
+          input={{
+            bg: cardBg,
+            value: search,
+            type: 'search',
+            onChange: (e) => setSearch(e.target.value),
+          }}
+        />
+      </Box>
+      <ModalBody>
+        <Tabs
+          variant="soft-rounded"
+          index={tab}
+          onChange={setTab}
+          display="flex"
+          flexDirection="column"
+        >
+          <TabList>
+            <TabButton>By Name</TabButton>
+            <TabButton>User ID</TabButton>
+          </TabList>
+
+          <TabPanels overflow="auto" minH="100px">
+            <TabPanel px={0}>
+              <QueryStatusLayout
+                query={nameQuery.query}
+                watch={nameQuery.query.data}
+                placeholder={<ResultPlaceholder />}
+                loading={nameQuery.query.isInitialLoading && <LoadingPanel size="sm" />}
+                container={(c) => <Flex direction="column" children={c} />}
+                error="Failed to search users"
+              >
+                {nameQuery.query.data?.map((user) => userItem(user))}
+              </QueryStatusLayout>
+            </TabPanel>
+            <TabPanel px={0}>
+              <QueryStatus
+                query={idQuery.query}
+                loading={idQuery.query.isInitialLoading && <LoadingPanel size="sm" />}
+                error="Failed to search user"
+              >
+                {idQuery.query.data && userItem(idQuery.query.data)}
+              </QueryStatus>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </ModalBody>
+    </>
+  );
+}
+
+function useSearchUsernameQuery() {
   const [search, setSearch] = useState<string>(null);
 
   const query = useQuery(['user_name', search], () => searchUser(search, 3, true), {
@@ -22,7 +142,7 @@ export function useSearchUsernameQuery() {
   };
 }
 
-export function useSearchUserIdQuery() {
+function useSearchUserIdQuery() {
   const [search, setSearch] = useState<string>(null);
 
   const query = useQuery(Keys.user(search), () => fetchUserInfo(search), {
@@ -36,7 +156,7 @@ export function useSearchUserIdQuery() {
   };
 }
 
-export function ResultPlaceholder() {
+function ResultPlaceholder() {
   return (
     <Placeholder icon={<Icon as={BiSearch} w={10} h={10} />} p="25px">
       No Result
